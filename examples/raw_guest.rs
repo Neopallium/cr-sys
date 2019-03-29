40d087c9d8447dae2fa4343eb892acdac9c21e24
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2019-03-30 01:03:20 +0800

    Show more of the API in the examples.

--- examples/raw_guest.rs
+++ examples/raw_guest.rs
@@ -2,24 +2,55 @@ extern crate cr_sys;
 
 use std::os::raw::{c_int};
 
+use std::time::Duration;
+use std::thread;
+
+mod raw_state;
+
+use raw_state::*;
+
 use cr_sys::*;
 
+use cr_sys::cr_op::*;
+
 #[no_mangle]
-pub fn cr_main(_ctx: &mut cr_plugin, _cr_op: c_int) -> c_int {
+pub fn cr_main(ctx: &mut cr_plugin, cr_op: cr_op) -> c_int {
     // Test "guest" feature.
     #[cfg(not(guest))]
     {
         // This is just to show if the guest was compiled with host-side code.
         println!("Guest compiled with host-side code.");
         // This code will only run if the "guest" feature is not used.
-        let mut plugin = cr_sys::cr_plugin::new();
+        let mut plugin = cr_plugin::new();
 
         let s_fullpath = std::ffi::CString::new("libtest.so").unwrap();
         unsafe { cr_plugin_load(&mut plugin, s_fullpath.as_ptr());}
         println!("- plugin = {:?}", plugin);
     }
 
-    println!("Hello from rust plugin. test2");
+    println!("Hello from rust plugin. test1");
+
+    match cr_op {
+        CR_LOAD => {
+            println!("Plugin load. version = {}", ctx.version);
+        },
+        CR_STEP => {
+            let version = ctx.version;
+            let state = unsafe { &mut *(ctx.userdata as *mut RawState) };
+            state.counter += 1;
+            println!("Plugin step. count = {}. version = {}", state.counter, version);
+
+            // slow down the printing.
+            thread::sleep(Duration::from_millis(200));
+        },
+        CR_UNLOAD => {
+            println!("Plugin unload. version = {}", ctx.version);
+        },
+        CR_CLOSE => {
+            println!("Plugin close. version = {}", ctx.version);
+        },
+    }
+
     return 0;
 }
 
--- examples/raw_host.rs
+++ examples/raw_host.rs
@@ -7,23 +7,40 @@ use std::thread;
 
 use cr_sys::*;
 
+mod raw_state;
+
+use raw_state::*;
+
 fn main() {
     // build the libraw_guest.so file from the samples of cr.h
     let mut plugin_name = env::current_exe().expect("Failed to get current path");
     println!("Path of this executable is: {}", plugin_name.display());
     plugin_name.set_file_name("libraw_guest.so");
 
+    // Plugin
+    let mut plugin = cr_sys::cr_plugin::new();
+    let mut state = RawState {
+        counter: 0,
+    };
+    plugin.userdata = &mut state as *mut _ as *mut ::std::os::raw::c_void;
+
     let plugin_name = plugin_name.to_str().unwrap();
     println!("Call cr_plugin_load(ctx, {:?})", plugin_name);
-    let mut plugin = cr_sys::cr_plugin::new();
     let s_fullpath = std::ffi::CString::new(plugin_name).unwrap();
     unsafe { cr_plugin_load(&mut plugin, s_fullpath.as_ptr()) };
 
+    let mut err_cnt = 0;
     loop {
         println!("Run Update:");
         let rc = unsafe { cr_plugin_update(&mut plugin, true)};
         println!("cr_plugin_update(ctx, true) = {}", rc);
-        if rc != 0 { break }
+        if rc != 0 {
+            println!("Plugin error: {:?}", plugin.failure);
+            err_cnt += 1;
+            if err_cnt > 10 {
+                break;
+            }
+        }
         thread::sleep(Duration::from_millis(200));
     }
     println!("Call cr_plugin_close(ctx)");
